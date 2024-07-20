55069e0d93becbcb8b1f646e15a49c49190244a0
malikali001 <malik.alihussain001@gmail.com>
2024-07-20 12:22:37 +0500

    add user authentication in goats

--- goat/views.py
+++ goat/views.py
@@ -1,4 +1,5 @@
 from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import (CreateView, DeleteView, DetailView, ListView,
                                   UpdateView)
 
@@ -7,19 +8,19 @@ from base.models.goats import Goat
 from .forms import GoatForm
 
 
-class GoatListView(ListView):
+class GoatListView(LoginRequiredMixin, ListView):
     model = Goat
     template_name = 'goat/list.html'
     context_object_name = 'goats'
 
 
-class GoatDetailView(DetailView):
+class GoatDetailView(LoginRequiredMixin, DetailView):
     model = Goat
     template_name = 'goat/detail.html'
     context_object_name = 'goat'
 
 
-class GoatCreateView(CreateView):
+class GoatCreateView(LoginRequiredMixin, CreateView):
     model = Goat
     form_class = GoatForm
     template_name = 'goat/form.html'
@@ -30,14 +31,19 @@ class GoatCreateView(CreateView):
         return super().form_valid(form)
 
 
-class GoatUpdateView(UpdateView):
+class GoatUpdateView(LoginRequiredMixin, UpdateView):
     model = Goat
     form_class = GoatForm
     template_name = 'goat/form.html'
     success_url = reverse_lazy('goat_list')
 
+    def form_valid(self, form):
+        if self.request.user.is_authenticated:
+            form.instance.manager = self.request.user
+        return super().form_valid(form)
 
-class GoatDeleteView(DeleteView):
+
+class GoatDeleteView(LoginRequiredMixin, DeleteView):
     model = Goat
     template_name = 'goat/confirm_delete.html'
     success_url = reverse_lazy('goat_list')
