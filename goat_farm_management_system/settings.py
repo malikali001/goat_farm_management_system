88163150a25f507ead5c63707a4d3afe4c800fb2
malikali001 <malik.alihussain001@gmail.com>
2024-11-24 22:17:04 +0500

    deploy

--- goat_farm_management_system/settings.py
+++ goat_farm_management_system/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-fg%b7!_^c^v&&dxyglm+6)j^f9n1lzds@s2hh%+yy*q=ig759+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
